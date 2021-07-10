ea670aac72b18eaee1692208d26dfaa68c2e861b
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-07-10 13:08:39 +0300

    user model created

---
+++ LINQ-Examples-NET6/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace LINQ_Examples_NET6.Models
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public User(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
